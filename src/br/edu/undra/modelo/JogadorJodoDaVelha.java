43cf1797a0ce07ae812af0bd2d37806d1dfe7891
Alexandre Siqueira <alematema@gmail.com>
2018-10-02 17:47:52 -0300

    <J extends Jogo>

--- src/br/edu/undra/modelo/JogadorJodoDaVelha.java
+++ src/br/edu/undra/modelo/JogadorJodoDaVelha.java
@@ -4,17 +4,17 @@ package br.edu.undra.modelo;
  *
  * @author alexandre
  */
-public class JogadorJodoDaVelha extends Jogador {
+public class JogadorJodoDaVelha<J extends Jogo> extends Jogador {
     
     public JogadorJodoDaVelha(String nome) {
         super(nome);
     }
     
-    public JogadorJodoDaVelha(Jogo jogo) {
+    public JogadorJodoDaVelha(J jogo) {
         super(jogo);
     }
 
-    public JogadorJodoDaVelha(String nome, Jogo jogo) {
+    public JogadorJodoDaVelha(String nome, J jogo) {
         super(nome, jogo);
     }
 
